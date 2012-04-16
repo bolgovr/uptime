5e06f220320b8956c3f9b58a3e51e24606ec7a1a
Bolgov Roman <bolgovr@gmail.com>
2012-04-16 18:20:58 +0300

    replace Ping stat collector with one that returned from Stat module according to checkType

--- app/api/routes/ping.js
+++ app/api/routes/ping.js
@@ -12,10 +12,10 @@ var Stat       = require('../../../models/stat');
 module.exports = function(app) {
 
   app.get('/pings/check/:id/:page?', function(req, res, next) {
-    Check.count({ _id: req.params.id}, function(err, nb_checks) {
-      if (err) return app.next(err);
-      if (!nb_checks) return app.next(new Error('failed to load check ' + req.params.id));
-      Ping.find({ check: req.params.id }).desc('timestamp').limit(50).skip((req.params.page -1) * 50).run(function(err, pings) {
+    Check.findById(req.params.id, function(err, check) {
+      if (err) return next(err);
+      if (!check) return next(new Error('failed to load check ' + req.params.id));
+      Stat(check.checkType).find({ check: req.params.id }).desc('timestamp').limit(50).skip((req.params.page -1) * 50).run(function(err, pings) {
         if (err) return next(err);
         res.json(pings);
       });
@@ -38,7 +38,7 @@ module.exports = function(app) {
         return res.send('Error: This check was already polled. No ping was created', 403);
       };
       var status = req.body.status === 'true';
-      Ping.createForCheck(status, req.body.time, check, req.body.name, req.body.error, function(err2, ping) {
+      Stat(check.checkType).createForCheck(status, req.body.time, check, req.body.name, req.body.error, function(err2, ping) {
         if (err2) {
           return res.send(err2.message, 500);
         }
